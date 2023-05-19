aeab1f55b2dbaf5dff3bfc1052080ca49d541ae0
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-05-19 15:42:44 -0700

    added inquirer list

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,28 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What is your goal in this challenge?',
+      name: 'variety',
+      choices: ['See if the code works', 'Play around', 'idk I am lost'],
+    },
+    {
+      type: 'list',
+      message: 'Does it work?',
+      name: 'answer',
+      choices: ['Yes!', 'No!'],
+    },
+];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
