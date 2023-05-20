05a8b2008cbd55ead26dad08fd869794652eaea2
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-05-19 22:53:15 -0700

    added functions for index.js page

--- Develop/index.js
+++ Develop/index.js
@@ -2,33 +2,86 @@
 
 // TODO: Create an array of questions for user input
 const inquirer = require('inquirer');
+
 const questions = [
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?',
+      message: 'Project title?',
     },
     {
-      type: 'checkbox',
-      message: 'What is your goal in this challenge?',
-      name: 'variety',
-      choices: ['See if the code works', 'Play around', 'idk I am lost'],
-    },
+        type: 'input',
+        name: 'description',
+        message: 'Project description?',
+      },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions?',
+      },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution info?',
+      },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'For questions(email)?',
+      },
+    {
+        type: 'input',
+        name: 'Github',
+        message: 'For questions(github)?',
+      },
+
     {
       type: 'list',
-      message: 'Does it work?',
-      name: 'answer',
-      choices: ['Yes!', 'No!'],
+      message: 'License?',
+      name: 'License',
+      choices: ['ISC', 'GNUPLv3', 'MIT'],
+    //   filter(val)
+    //     return val.toLowerCase()
     },
 ];
 
+function runQuery(){
+    return inquirer.createPromptModule(questions)
+    .then((answers)=> {
+        console.log(answers)
+        return answers
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
 
+runQuery()
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    FileSystem.writeToFile(fileName,data,error => {
+        if(error) {
+            return console.log(error)
+        }
+        console.log('Your READMe.md file has beengenerated!')
+    })
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+async function init() {
+    try {
+        
+        // ask user inquirer questions
+        const userInput = await inquirer.createPromptModule(questions);
+        console.log('your responses: ', userInput);
+        // console.log('Your GitHub user info: ', userData);
 
-// Function call to initialize app
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// // Function call to initialize app
 init();
