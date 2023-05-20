058c122cea47bd59ab9b39a55e60dd70b543af39
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-05-19 21:08:31 -0700

    added to generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  return ``
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +15,33 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Table of Content
+  - [Project description](#Description)
+  - [Usage](#Usage)
+  - [Contribution](#Contribution)
+  - [Installation](#Insatllation)
+  - [Questions](#Questions)
+  - [License](#License)
+
+  ## Description
+  ${answers.usage}
+
+  ## Usage
+  ${answers.usage}
+
+  ## Installation
+  ${answers.installation}
+
+  ## Contribution
+  ${answers.contribution}
+
+  ## Questions
+  ${answers.email}
+  ${answers.github}
+
+  ## License
+  ${answers.license}
 
 `;
 }
